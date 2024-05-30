e8409b2d2ec951c99ae1d0d98af69dc2182a4e5e
AjwadG <ajwadelgandouz@yahoo.com>
2024-05-30 23:30:44 +0200

    6. Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -16,3 +16,17 @@ def hash_password(password: str) -> bytes:
         bytes: The hashed password.
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate a password against a hashed password.
+
+    Args:
+        hashed_password (bytes): The hashed password.
+        password (str): The password to validate.
+
+    Returns:
+        bool: True if the password is valid, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
