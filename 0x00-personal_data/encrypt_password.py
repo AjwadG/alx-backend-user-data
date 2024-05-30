09b244ca1582097dfb24690197af0dabaa7f25aa
AjwadG <ajwadelgandouz@yahoo.com>
2024-05-30 23:24:13 +0200

    5. Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+encrypt_password module for hashing passwords
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hash a password using bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The hashed password.
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
