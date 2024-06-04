c29269d1f08f4db78e90440188592ea840de52be
AjwadG <ajwadelgandouz@yahoo.com>
2024-06-04 15:47:12 +0200

    7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,14 @@ from models.user import User
 
 class BasicAuth(Auth):
     """ BasicAuth class """
+
+    def extract_base64_authorization_header(self, authorization_header: str
+                                            ) -> str:
+        """  Extract base64 authorization header """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
